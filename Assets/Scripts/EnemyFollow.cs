3e121cd566324b10d575383ffa8b76a9b7b117f4
DubchaKK <kirilldubchak@gmail.com>
2020-10-04 01:38:25 +0300

    Add Enemy
    He can move and rotate in your tank

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        var newRotation = Quaternion.LookRotation(transform.position - target.position, Vector3.forward);
+        newRotation.x = 0.0f;
+        newRotation.y = 0.0f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 8);
+
+    }
+}
