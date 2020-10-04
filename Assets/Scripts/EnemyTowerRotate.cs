773f9cd61ee98cba3caa7d30832fe5e136378a72
DubchaKK <kirilldubchak@gmail.com>
2020-10-04 12:33:20 +0300

    Animation and new bot ->
    -Animation for bots;
    -Bot that go in Spots;

---
+++ Assets/Scripts/EnemyTowerRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyTowerRotate : MonoBehaviour
+{
+    public Transform target;
+
+    void FixedUpdate()
+    {
+        var newRotation = Quaternion.LookRotation(transform.position - target.position, Vector3.forward);
+        Vector3 direction = target.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 16);
+    }
+}
+
