ac933a101b638e81b634f295799b5eff218cff17
DubchaKK <kirilldubchak@gmail.com>
2020-10-05 23:28:00 +0300

    Add bad First Aid

---
+++ Assets/Scripts/FirstAid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstAid : MonoBehaviour
+{
+	public bool NeedAdd = false;
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			Destroy(gameObject, 7f);
+			NeedAdd = true;
+		}
+	}
+}
--- Assets/Scripts/HPBarScript.cs
+++ Assets/Scripts/HPBarScript.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 
 public class HPBarScript : MonoBehaviour
 {
-    public int HealthINT;
+    public float HealthINT;
     public Slider slider;
     public Text HP;
     public string HealthSTR;
 
     void FixedUpdate()
     {
+        FirstAid target = (FirstAid)FindObjectOfType(typeof(FirstAid));
+        FirstAid FirstAid = target.GetComponent<FirstAid>();
         HealthSTR = HealthINT.ToString();
         slider.value = HealthINT;
         HP.text = HealthSTR;
+        if (FirstAid.NeedAdd == true)
+        {
+            HealthINT = 100;
+        }
     }
 
 
