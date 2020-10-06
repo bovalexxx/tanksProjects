78b6130a24591c9dfb38ce6e7d748b3028355195
DubchaKK <kirilldubchak@gmail.com>
2020-10-06 18:31:29 +0300

    Bonus Rocket

---
+++ Assets/Scripts/BonusRocket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusRocket : MonoBehaviour
+{
+	public int RocketHave = 0;
+	public GameObject otherObj;
+
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "BonusRocket")
+		{
+			Destroy(otherObj);
+			RocketHave = RocketHave + 1;
+		}
+	}
+}
---
+++ Assets/Scripts/RocketBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBullet : MonoBehaviour
+{
+    public GameObject RocketBullet2;
+    public int RocketbulletSpeed;
+
+    void Update()
+    {
+        BonusRocket target = (BonusRocket)FindObjectOfType(typeof(BonusRocket));
+        BonusRocket BonusRocket = target.GetComponent<BonusRocket>();
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (BonusRocket.RocketHave >= 1)
+            {
+                GameObject projectile = Instantiate(RocketBullet2, transform.GetChild(0));
+                projectile.transform.SetParent(null);
+                projectile.GetComponent<Rigidbody2D>().AddForce(transform.up * RocketbulletSpeed);
+            }
+        }
+    }
+}
