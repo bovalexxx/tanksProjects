d163014afd243a9d53c958c5d4a8694bbd29b607
DubchaKK <kirilldubchak@gmail.com>
2020-10-06 13:09:01 +0300

    ->
    -Bots HP
    -Hide bullet when touch objects

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject other;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+			Destroy(other);
+	}
 }
---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int EnemyHP = 100;
+
+    public GameObject other;
+
+    void FixedUpdate()
+    {
+        if (EnemyHP <= 0)
+        {
+            Destroy(other);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            EnemyHP = EnemyHP - 25;
+        }
+    }
+}
